abdd0b736e89dae67d965939cf24897487eb53e8
tobias pflug <tobias.pflug@gmail.com>
2014-10-02 00:01:05 +0200

    Error handling : Use ErrorT

--- src/Main.hs
+++ src/Main.hs
@@ -4,7 +4,6 @@
 
 import GHC.Generics
 import System.Environment (getArgs)
-import System.Exit
 import Data.Aeson hiding (encode)
 import Data.Yaml
 import Data.List
@@ -12,7 +11,7 @@ import System.Directory (doesFileExist, findExecutable, getCurrentDirectory, get
 import System.FilePath.Posix
 import Hoogle (defaultDatabaseLocation, mergeDatabase)
 import Control.Applicative
-import Control.Monad (filterM, liftM, unless)
+import Control.Monad.Error
 import Data.Maybe (isJust)
 
 import Hoobuddy
@@ -37,36 +36,37 @@ defaultPkgs = words "Cabal.hoo array.hoo base.hoo binary.hoo bytestring.hoo cont
 
 help :: IO ()
 help = putStrLn $
-    unlines [ "Usage : hoobuddy [deps|build] <cabal-file>"
+    unlines [ "Usage : hoobuddy [deps|fetch] <cabal-file>"
             , "                 [--help]"
             , "                 [--default]"
             , ""
             , "deps         list configured dependencies"
-            , "build        do stuff yet to be defined"
+            , "fetch        fetch and merge documentation databases"
             , ""
             , "--default    prints the default configuration"
             , "--help       prints this help"
             ]
 
+type HoobuddyAction = ErrorT String IO
+
 main :: IO ()
 main = do
-    exitIfHoogleMissing
     conf <- loadConfig
     args <- getArgs
-    run conf args where
-        run _ ["deps", file]          = deps file
-        run conf  ["build", file]     = build file conf
-        run _ ["--help"]              = help
-        run _ ["--default"]           = defaultConfig >>= \x -> putStrLn $ unpack $ encode x
-        run _ _ = do
-            help
-            exitWith (ExitFailure 1)
+    ret <- runErrorT $ runHoobuddy conf args
+    either putStrLn (\_ -> putStrLn "") ret
 
--- | Exits with error code if hoogle isn't installed
-exitIfHoogleMissing :: IO ()
-exitIfHoogleMissing = do
-    hoogleInstalled <- liftM isJust (findExecutable "hoogle")
-    unless hoogleInstalled (putStrLn hoogleMissingError >> exitWith (ExitFailure 1))
+runHoobuddy :: Hoobuddy -> [String] -> HoobuddyAction ()
+runHoobuddy cfg args = do
+    hoogleInstalled <- liftM isJust (liftIO $ findExecutable "hoogle")
+    unless hoogleInstalled (throwError hoogleMissingError)
+    run cfg args
+        where
+            run _     ["deps", file]      = liftIO $ deps file
+            run conf  ["fetch", file]     = build file conf
+            run _     ["--help"]          = liftIO help
+            run _     ["--default"]       = liftIO $ defaultConfig >>= \x -> putStrLn $ unpack $ encode x
+            run _ _                       = liftIO help
 
 
 -- | Loads configuration from file or uses defaults
@@ -89,28 +89,29 @@ decodeConfig = do
     return $ either (const Nothing) Just parseResult
 
 
-build :: FilePath -> Hoobuddy -> IO ()
+build :: FilePath -> Hoobuddy -> HoobuddyAction ()
 build cabalFile conf = do
-    pkgs <- map (++ ".hoo") <$> getDeps cabalFile
-    dbs <- getHooDatabases (databases conf)
+    pkgs <- map (++ ".hoo") <$> liftIO (getDeps cabalFile)
+    dbs <- liftIO $ getHooDatabases (databases conf)
 
     let allPkgs = (++) pkgs (if useBase conf then defaultPkgs else custom conf)
     let available = allPkgs `intersect` dbs
     let missing = filter (`notElem` available) allPkgs
 
-    printInfo "Fetching databases for: " missing
-    hoogleFetch missing >>= \x -> case x of
-        Right _             -> return ()
-        Left (code, stderr) -> putStrLn ("hoogle exited with error:\n" ++ stderr) >> exitWith code
+    liftIO $ if null missing
+     then putStrLn "No data needs to be fetched"
+     else printInfo "Fetching databases for: " missing
+    fetchOp <- liftIO $ hoogleFetch missing
+    either (\_ -> throwError "Error executing hoogle") (\_ -> return ()) fetchOp
 
-    putStrLn "Merging databases ..."
-    currDir <- getCurrentDirectory
-    existingDbs <- filterM doesFileExist (fmap (databases conf </>) allPkgs)
-    mergeDatabase  existingDbs (currDir </> "default.hoo")
+    liftIO $ putStrLn "Merging databases ..."
+    currDir <- liftIO getCurrentDirectory
+    existingDbs <- liftIO $ filterM doesFileExist (fmap (databases conf </>) allPkgs)
+    liftIO $ mergeDatabase existingDbs (currDir </> "default.hoo")
+    liftIO $ putStrLn "Success: default.hoo"
 
 -- | Pretty printer for info output
 printInfo :: String -> [String] -> IO ()
-printInfo _ [] = return ()
 printInfo str xs = putStrLn $ str ++ "[" ++ intercalate "," xs ++ "]"
 
 
